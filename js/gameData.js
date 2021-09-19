0024296dd7363d61459cf403a4a710960df8a2bb
cheenlam <redsinsue@gmail.com>
2021-09-19 15:30:40 +0800

    no message

---
+++ js/gameData.js
@@ -0,0 +1,141 @@
+var guideCards = [{
+        cardSrc: [
+            'images/card/card_10.png',
+            'images/card/card_20.png'
+        ],
+        fraction: '10땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_9.png',
+            'images/card/card_19.png'
+        ],
+        fraction: '9땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_8.png',
+            'images/card/card_18.png'
+        ],
+        fraction: '8땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_7.png',
+            'images/card/card_17.png'
+        ],
+        fraction: '7땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_6.png',
+            'images/card/card_16.png'
+        ],
+        fraction: '6땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_5.png',
+            'images/card/card_15.png'
+        ],
+        fraction: '5땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_4.png',
+            'images/card/card_14.png'
+        ],
+        fraction: '4땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_3.png',
+            'images/card/card_13.png'
+        ],
+        fraction: '3땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_2.png',
+            'images/card/card_12.png'
+        ],
+        fraction: '2땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_11.png'
+        ],
+        fraction: '1땡',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_8.png'
+        ],
+        fraction: '9끗 (갑오)',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_7.png'
+        ],
+        fraction: '8끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_6.png'
+        ],
+        fraction: '7끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_5.png'
+        ],
+        fraction: '6끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_4.png'
+        ],
+        fraction: '5끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_3.png'
+        ],
+        fraction: '4끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_2.png'
+        ],
+        fraction: '3끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_3.png',
+            'images/card/card_9.png'
+        ],
+        fraction: '2끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_2.png',
+            'images/card/card_9.png'
+        ],
+        fraction: '1끗',
+    },
+    {
+        cardSrc: [
+            'images/card/card_1.png',
+            'images/card/card_9.png'
+        ],
+        fraction: '0끗 (망통)',
+    },
+]
